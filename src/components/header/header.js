c784d0a9608ca4630a14d1f589645ba815ed3548
Ruslan04071996 <ruslan040796@mail.ru>
2020-08-25 13:01:51 +0300

    Очистка формы после ее отправки

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,6 +15,9 @@ state = {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.addItem(this.state.label);
+        this.setState({
+            label: ''
+        })
     }
     render() {
         return (
@@ -22,7 +25,7 @@ state = {
                 <h1>todos</h1>
                 <form className='form-inline' onSubmit = {this.onSubmit}>
                     <div className='input-block-el'>
-                        <input className='new-todo' placeholder='What needs to be done ?' onChange={this.onLabelChange} autoFocus /></div>
+                        <input className='new-todo' placeholder='What needs to be done ?' onChange={this.onLabelChange} value = {this.state.label} autoFocus /></div>
                     <ButtonAdd addItem={this.onSubmit} />
                 </form>
             </header>
