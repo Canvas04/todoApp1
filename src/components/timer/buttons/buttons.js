9e65b82c18f05e0ea0052f343dca694802a93b04
Ruslan04071996 <ruslan040796@mail.ru>
2020-09-19 23:16:22 +0300

    Добавил нужные пропсы

--- src/components/timer/buttons/buttons.js
+++ src/components/timer/buttons/buttons.js
@@ -16,5 +16,5 @@ export default function Buttons({handleClick,timingEvents}) {
 
 Buttons.propTypes = {
     handleClick: Proptypes.func.isRequired,
-    
+    timingEvents: Proptypes.arrayOf(Proptypes.object).isRequired
 }
